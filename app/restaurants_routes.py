6e93ca63f83dc097ffe39ab46aa64b6facdf8c09
Clara Lecroisey <clalecroisey@gmail.com>
2023-12-07 18:21:04 +0100

    feat: return tokyo restaurants from endpoint (hardcoded for now)

--- app/restaurants_routes.py
+++ app/restaurants_routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, jsonify
+import requests
+from flask import Blueprint, current_app, jsonify
 
 restaurants_blueprint = Blueprint("restaurants", __name__, url_prefix="/restaurants")
 
@@ -6,6 +7,39 @@ restaurants_blueprint = Blueprint("restaurants", __name__, url_prefix="/restaura
 @restaurants_blueprint.route("", methods=["GET"])
 def get_restaurants_locations():
     try:
-        return jsonify(restaurants=[])
+        # Tokyo location
+        data = {
+            "includedTypes": ["restaurant"],
+            "locationRestriction": {
+                "circle": {
+                    "center": {"latitude": 35.709026, "longitude": 139.731992},
+                    "radius": 500.0,
+                }
+            },
+        }
+
+        response = requests.post(
+            url="https://places.googleapis.com/v1/places:searchNearby",
+            json=data,
+            headers={
+                "Content-Type": "application/json",
+                "X-Goog-Api-Key": current_app.config["GOOGLE_PLACES_API_KEY"],
+                "X-Goog-FieldMask": "places.displayName",
+            },
+        )
+
+        data = response.json()
+
+        if data:
+            restaurants = data["places"]
+        else:
+            # When there are no results, the response is an empty object
+            restaurants = []
+
+        return jsonify(restaurants=restaurants)
     except Exception as e:
-        return jsonify(error=str(e)), 400
+        current_app.logger.error(e)
+        return (
+            jsonify(error="An unexpected error happened, please try again later"),
+            400,
+        )
